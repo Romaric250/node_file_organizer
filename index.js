a9cb56d459ddaf927c803192df7413302b89e37a
Romaric250 <lonfonyuyromeo@gmail.com>
2024-12-10 22:42:23 +0100

    mking some progrss

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 
+const fs = require('node:fs')
 
-console.log("node here")
+
+
+fs.readdir(".node_file_organizer",(error, data) => {
+    if(error){
+        console.log(error)
+        return
+    }
+    data.forEach(file=> console.log(file))
+})
