4a2a5e93d7941ab8ce4baeface1bf076fceee9df
Romaric250 <lonfonyuyromeo@gmail.com>
2024-12-29 14:36:04 +0100

    updated the path files

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-
 const fs = require('node:fs')
 
 
-
 const dirdata = {}
+let filedata = []
 
 
+const data = fs.readdirSync('./tester', (error, files) => {
+    if (error) {
+        console.log(error.message)
 
 
+    }
+
+    filedata = [...files]
+    return filedata
+
+})
 
 
-console.log("dir data here", dirdata)
+data.map((file) => {
+    const splitfile = file.split('.')
+    console.log("splitfile here", splitfile)
+
+
+    dirdata[`${splitfile[1]}`] = splitfile[0]
+})
+
+console.log("dat here", data)
+
+
+for (const dat in Object.keys(dirdata)) {
+
+}
+
+
+console.log("dir data here and more", data, dirdata)
 
 
 
