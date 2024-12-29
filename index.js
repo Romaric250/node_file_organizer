c78ac3f7af1576e3e27aefc23a496327561cb847
Romaric250 <lonfonyuyromeo@gmail.com>
2024-12-29 13:25:08 +0100

    cleaning updates

--- index.js
+++ index.js
@@ -2,47 +2,12 @@
 const fs = require('node:fs')
 
 
-let files_in_dir = []
-fs.stat('./tester', (error, data) => {
-   if(error){
-    console.log(error)
-
-   }
-   console.log("data here", data)
-}
-)
 
 const dirdata = {}
 
-dirdata['romy'] = "30px"
-
-fs.readdir('./tester', (error, files)=> {
-    if(error){
-            console.log(error)
-        }
-
-        console.log('files here', files)
-
-        files_in_dir = files
 
 
-        files_in_dir.forEach((file) => 
-{
-            file_extension = file.split('.')
 
-            console.log('some data', file_extension)
-
-
-            // console.log('extensions', file_extension[1])
-}
-        
-        
-        
-        )
-
-        
-
-})
 
 
 console.log("dir data here", dirdata)
