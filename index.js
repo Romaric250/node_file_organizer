4d88231badba0fa742cf45f281ebab3f8a6cb29a
Romaric250 <lonfonyuyromeo@gmail.com>
2024-12-29 10:26:27 +0100

    refactored some functions

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 
 const fs = require('node:fs')
 
-fs.readdir(".node_file_organizer",(error, data) => {
-    if(error){
-        console.log("error here", error.message)
-        return
-    }
 
-    console.log(data)
-    
+let files_in_dir = []
+fs.stat('./tester', (error, data) => {
+   if(error){
+    console.log(error)
+
+   }
+   console.log("data here", data)
+}
+)
+
+const dirdata = {}
+
+dirdata['romy'] = "30px"
+
+fs.readdir('./tester', (error, files)=> {
+    if(error){
+            console.log(error)
+        }
+
+        console.log('files here', files)
+
+        files_in_dir = files
+
+
+        files_in_dir.forEach((file) => 
+{
+            file_extension = file.split('.')
+
+            console.log('some data', file_extension)
+
+
+            // console.log('extensions', file_extension[1])
+}
+        
+        
+        
+        )
+
+        
 
 })
+
+
+console.log("dir data here", dirdata)
+
+
+
+
+
